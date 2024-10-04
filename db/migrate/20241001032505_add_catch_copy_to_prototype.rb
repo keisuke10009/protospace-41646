44a3510416b558ffe111b79729427c157d45bf31
keisuke10009 <hiruta40@hotmail.com>
2024-10-04 17:52:27 +0900

    再デプロイ用に修正

--- db/migrate/20241001032505_add_catch_copy_to_prototype.rb
+++
@@ -1,5 +0,0 @@
-class AddCatchCopyToPrototype < ActiveRecord::Migration[7.0]
-  def change
-    add_column :prototypes, :catch_copy, :string
-  end
-end
--- db/migrate/20241001033643_prototype.rb
+++
@@ -1,5 +0,0 @@
-class Prototype < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :prototypes
-  end
-end
